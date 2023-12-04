b894710e2d5fc940116170e6a3988adff3897bd7
Jose Armando Imbacuan Morillo (jaim79) <joseimbacuan705@gmail.com>
2023-12-04 06:01:05 -0500

    first commit

---
+++ src/main/java/Persona.java
@@ -0,0 +1,48 @@
+public class Persona {
+    private String Nombre;
+    private String apellido;
+    private String telefono;
+
+    public Persona(){
+
+    }
+
+    public Persona(String nombre, String apellido, String telefono) {
+        Nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "Nombre='" + Nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/TestPersona.java
@@ -0,0 +1,10 @@
+public class TestPersona {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        persona1.setApellido("Jose");
+        persona1.setApellido("Imbacuan");
+        persona1.setTelefono("3152002485");
+
+        System.out.println("datos: " + persona1.toString());
+    }
+}
