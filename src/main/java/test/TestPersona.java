9a1c1a51f74d3b4d16ec043714c011a71c06b3cf
Jose Armando Imbacuan Morillo (jaim79) <joseimbacuan705@gmail.com>
2023-12-13 05:38:05 -0500

    nuevos cambios alenjandro

--- src/main/java/test/TestPersona.java
+++ src/main/java/test/TestPersona.java
@@ -2,13 +2,13 @@ package test;
 
 import models.Persona;
 
-public class TestPersona {
+public class
+TestPersona {
     public static void main(String[] args) {
         Persona persona1 = new Persona();
         persona1.setApellido("Jose");
         persona1.setApellido("Imbacuan");
         persona1.setTelefono("3152002485");
-
         System.out.println("datos: " + persona1.toString());
     }
 }
